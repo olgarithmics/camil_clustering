c7a3346061fa945220729490a172b91d4dbdbfe0
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2023-11-06 20:08:19 +0000

    non deterministic

--- run.py
+++ run.py
@@ -14,8 +14,8 @@ def set_seed(seed: int = 42) -> None:
   np.random.seed(seed)
   tf.random.set_seed(seed)
   tf.experimental.numpy.random.seed(seed)
-  os.environ['TF_DETERMINISTIC_OPS'] = '1'
-  os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+  # os.environ['TF_DETERMINISTIC_OPS'] = '1'
+  # os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
   #os.environ['TF_DISABLE_SPARSE_SOFTMAX_XENT_WITH_LOGITS_OP_DETERMINISM_EXCEPTIONS']='1'
 
 
