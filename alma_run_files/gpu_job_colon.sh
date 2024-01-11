f19b5b0a8ba7c4bf3370006b41c9d1af9ee44a12
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2024-01-11 13:18:43 +0000

    alma

--- alma_run_files/gpu_job_colon.sh
+++ alma_run_files/gpu_job_colon.sh
@@ -17,5 +17,5 @@ cd /home/ofourkioti/Projects/camil_clustering/
 #python run.py --dataset camelyon --experiment_name phi --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv --csv_files camelyon_csv_files/splits_0.csv  --epoch 100 --save_dir cam_Saved_model --lambda1 0.00001
 #python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
 for i in {0..4};
-do python run.py --experiment_name colon_sb --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon_feats/h5_files/ --label_file label_files/colon_data.csv --csv_file colon_csv_splits/splits_${i}.csv  --epoch 100  --k_sample 8 --single_branch;
+do python run.py --experiment_name colon_sb --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon_feats/h5_files/ --label_file label_files/colon_data.csv --csv_file colon_csv_splits/splits_${i}.csv  --epoch 100  --k_sample 5 --single_branch;
 done
