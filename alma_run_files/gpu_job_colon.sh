c178790e78c344dc6d6f247b522a9c0f23c20e29
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2024-01-11 13:14:14 +0000

    alma

--- alma_run_files/gpu_job_colon.sh
+++ alma_run_files/gpu_job_colon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=test_gpu
-#SBATCH --output=/home/ofourkioti/Projects/camil_clustering/results/colon_mb_test.txt
+#SBATCH --output=/home/ofourkioti/Projects/camil_clustering/results/colon_sb.txt
 #SBATCH --error=/home/ofourkioti/Projects/camil_clustering/results/error.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -12,10 +12,10 @@ module use /opt/software/easybuild/modules/all/
 module load Mamba
 source ~/.bashrc
 conda activate exp_env
-cd /home/ofourkioti/Projects/SAD_MIL/
+cd /home/ofourkioti/Projects/camil_clustering/
 
 #python run.py --dataset camelyon --experiment_name phi --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/camelyon_data/tf_feats_256/resnet_feats/h5_files --label_file label_files/camelyon_data.csv --csv_files camelyon_csv_files/splits_0.csv  --epoch 100 --save_dir cam_Saved_model --lambda1 0.00001
 #python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
 for i in {0..4};
-do python run.py --experiment_name colon_mb --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon_feats/h5_files/ --label_file label_files/colon_data.csv --csv_file colon_csv_splits/splits_${i}.csv  --epoch 100 --topk 5 --subtyping;
+do python run.py --experiment_name colon_sb --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/colon_feats/h5_files/ --label_file label_files/colon_data.csv --csv_file colon_csv_splits/splits_${i}.csv  --epoch 100 --topk 5 --subtyping;
 done
