367df47a24262adfb99d8e2fe44fe79515dce64f
admin_ofourkioti <admin_ofourkioti@30357CMBDTL.icr.ac.uk>
2024-01-18 22:21:09 +0000

    clam multi-label

--- alma_run_files/gpu_job_sarcoma.sh
+++ alma_run_files/gpu_job_sarcoma.sh
@@ -15,5 +15,5 @@ conda activate exp_env
 cd /home/ofourkioti/Projects/camil_clustering/
 
 
-python run.py  --experiment_name lipo --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/SAR/feats/h5_files --label_file label_files/multi_lipo.csv --csv_files lipo_splits  --epoch 100 --save_dir SAR_Saved_model
+python run.py  --experiment_name lipo --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/tmi/SAR/feats/h5_files --label_file label_files/multi_lipo.csv --csv_file lipo_splits  --epoch 100 --save_dir SAR_Saved_model
 #python run_simclr.py --simclr_path  lipo_SIMCLR_checkpoints --feature_path /data/scratch/DBI/DUDBI/DYNCESYS/OlgaF/multi_magnification_project/SAR_data/simclr_imgs/h5_files/  --csv_file lipo_csv_files/splits_0.csv --simclr_batch_size 1024
